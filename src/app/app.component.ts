46f1bb6e68d14878805d89d1fddb14d046c6af65
Daniel Hernández Alcojor <dhalcojor@gmail.com>
2023-02-28 14:41:08 +0100

    Add Angular Material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @Component({
-  selector: 'app-root',
+  selector: 'alcojor-root',
+  standalone: true,
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  imports: [
+    RouterOutlet,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+  ],
 })
 export class AppComponent {
-  title = 'alcojor';
+  readonly title = 'Alcojor';
 }
